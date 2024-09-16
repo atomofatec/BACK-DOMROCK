0e7f0e3c607af22e65cca027c75bbbd46e331f6c
Rebeca Gama <rebeca.moraes@fatec.sp.gov.br>
2024-09-16 10:40:25 -0300

    <Sprint I> feat: Interface interativa
    Interface de interação no CMD

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 from files.preprocess import load_and_preprocess_data
 from files.embeddings import gerar_embeddings
-from files.faiss_search import criar_faiss_index
-from files.faiss_search import buscar_por_produto
+from files.faiss_search import criar_faiss_index, buscar_por_produto
 from files.text_generation import gerar_resposta_por_produto
 from sentence_transformers import SentenceTransformer
 
-# criar ambiente virtual em python (opcional)
-# instalar os pacotes com o comando abaixo (obrigatório):
-# pip install -r requirements.txt
+def main():
+    # Carregar e pré-processar os dados
+    df = load_and_preprocess_data(r'data\chat_data.csv')
 
-# carregar e pré-processar os dados
-df = load_and_preprocess_data(r'data\chat_data.csv')
+    model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+    # Definir as colunas de texto para geração de embeddings
+    text_columns = ['product_name_processado', 'product_brand_processado', 'site_category_lv1_processado', 
+                    'site_category_lv2_processado', 'review_title_processado', 'recommend_to_a_friend_processado', 
+                    'review_text_processado', 'reviewer_gender_processado', 'reviewer_state_processado']
 
-# Definir as colunas de texto para geração de embeddings
-text_columns = ['product_name_processado', 'product_brand_processado', 'site_category_lv1_processado', 
-                'site_category_lv2_processado', 'review_title_processado', 'recommend_to_a_friend_processado', 
-                'review_text_processado', 'reviewer_gender_processado', 'reviewer_state_processado']
+    # Gerar embeddings
+    embeddings_dict = gerar_embeddings(df, text_columns)
 
-# gerar embeddings
-embeddings_dict = gerar_embeddings(df, text_columns)
+    # Criar índice faiss para os embeddings
+    index = criar_faiss_index(embeddings_dict['review_text_processado'])
 
-# criar índice faiss para os embeddings
-index = criar_faiss_index(embeddings_dict['review_text_processado'])
+    print("Bem-vindo ao chatbot! Digite 'sair' para encerrar.")
 
-# consultar por comentário similar (o comentário está sendo passado diretamente no código, criar interface no prompt para interação)
-consulta_produto = "Copo Acrílico Com Canudo 500ml Rocie"
-resultados = buscar_por_produto(df, index, consulta_produto, model, k=10)
+    while True:
+        consulta_produto = input("Qual a sua dúvida? ")
 
-# gerar resposta com gpt2
-if resultados:
-    resposta_gerada = gerar_resposta_por_produto(resultados)
-    print(f"Resposta gerada: {resposta_gerada}")
-else:
-    print("Produto não encontrado ou sem avaliações.")
+        if consulta_produto.lower() == 'sair':
+            print("Encerrando o chatbot. Até mais!")
+            break
+
+        # Consultar por comentário similar
+        resultados = buscar_por_produto(df, index, consulta_produto, model, k=10)
+
+        # Gerar resposta com GPT-2
+        if resultados:
+            resposta_gerada = gerar_resposta_por_produto(resultados)
+            print(f"Resposta gerada: {resposta_gerada}")
+        else:
+            print("Produto não encontrado ou sem avaliações.")
+
+if __name__ == "__main__":
+    main()

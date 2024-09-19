17689b887ea4c5dbf6428d696922b03af02c0665
ritahecht <rita.hecht@outlook.com>
2024-09-19 11:33:29 -0300

    <Sprint I> refactor: correção das pastas
    pastas e arquivos realocados

--- files/embeddings.py
+++ files/embeddings.py
@@ -1,9 +1,9 @@
 from sentence_transformers import SentenceTransformer
 
-# modelo pré-treinado de embeddings de sentenças
+# Modelo pré-treinado de embeddings de sentenças
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 
-# função para gerar e armazenar embeddings para cada coluna de texto
+# Função para gerar e armazenar embeddings para cada coluna de texto
 def gerar_embeddings(df, text_columns):
     embeddings_dict = {}
     
--- files/text_generation.py
+++ files/text_generation.py
@@ -1,23 +1,26 @@
 from transformers import pipeline
 
-# modelo de geração de texto pré-treinado (gpt2)
+# Modelo de geração de texto pré-treinado (GPT-2)
 generator = pipeline('text-generation', model='gpt2')
 
-# função que gera o texto com base no comentário fornecido e retorna as notas relacionadas a ele
-# max_length define o tamanho da mensagem e num_return_sequences define quantidade de respostas
-# necessário aprimoramento para respostas coerentes
 def gerar_resposta_por_produto(resultados):
     if not resultados:
         return "Nenhuma avaliação encontrada para o produto."
     
-    nome_produto = resultados[0]['produto']  
+    nome_produto = resultados[0]['produto']
     
     notas = [str(resultado['nota']) for resultado in resultados]
+    comentarios = [resultado['comentário'] for resultado in resultados]
     
     notas_texto = ", ".join(notas)
+    comentarios_texto = " ".join(comentarios)
     
-    prompt = f"O produto {nome_produto} recebeu as seguintes notas de diferentes usuários: {notas_texto}. Gere um resumo dessas avaliações."
+    prompt = (
+        f"O produto '{nome_produto}' recebeu as seguintes notas de diferentes usuários: {notas_texto}. "
+        f"Aqui estão alguns comentários dos usuários: {comentarios_texto}. "
+        "Por favor, gere um resumo claro e conciso dessas avaliações, destacando os pontos positivos e negativos."
+    )
     
-    resposta = generator(prompt, max_length=100, num_return_sequences=1)
+    resposta = generator(prompt, max_new_tokens=100, num_return_sequences=1)
 
     return resposta[0]['generated_text']

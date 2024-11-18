92fc5bffde102ad184f9f7721230166c7d1e7f5a
João Gabriel <joao.mira@fatec.sp.gov.br>
2024-11-18 07:46:47 -0300

    <Sprint III> feat: cors
    rotas corrigidas com uso do cors para requisição no front

--- src/server.py
+++ src/server.py
@@ -1,40 +1,37 @@
 from flask import Flask, request, jsonify
 from main import handle_query
+from flask_cors import CORS
 
 # instancia o servidor flask
 app = Flask(__name__)
 
-# lista para armazenar as respostas
-chat_answers = []
+# Habilita CORS
+CORS(app)
 
 # rota inicial
+
+
 @app.route("/")
 def init_server():
     return "Servidor sendo executado."
 
-# rota para recuperar as respostas
-@app.route("/chat-answers", methods=["GET"])
-def return_answers():
-    return jsonify(chat_answers), 200
-
 # rota para fazer as perguntas
+
+
 @app.route("/ask-chat", methods=["POST"])
 def ask_chat():
     data = request.get_json()
-    pergunta = data.get("pergunta") # pergunta feita pelo usuário
+    pergunta = data.get("pergunta")  # pergunta feita pelo usuário
 
     if not pergunta:
         return jsonify({"error": "Pergunta não fornecida"}), 400
 
-    resposta = handle_query(pergunta) # executa a função handle_query passando a pergunta como parâmetro para obter a resposta
-
-    chat_answers.append({
-        "pergunta": pergunta,
-        "resposta": resposta
-    }) # formata o json
+    # Chama a função handle_query para processar a pergunta
+    resposta = handle_query(pergunta)
 
     return jsonify({"resposta": resposta}), 200
 
+
 # executa o servidor
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
